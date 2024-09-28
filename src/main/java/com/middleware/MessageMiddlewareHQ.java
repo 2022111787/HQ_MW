1c684affd5a86401c0cde3bba79665af774b203d
孙迪雅 <1783252994@qq.com>
2024-09-28 12:25:55 +0800

    添加讲师和现场工作人员消息

--- src/main/java/com/middleware/MessageMiddlewareHQ.java
+++ src/main/java/com/middleware/MessageMiddlewareHQ.java
@@ -9,6 +9,8 @@ public class MessageMiddlewareHQ {
         ManagerObserver manager = new ManagerObserver("公司经理");
         ManagerObserver instructor = new ManagerObserver("讲师");
         ManagerObserver student = new ManagerObserver("学员");
+        ManagerObserver executor = new ManagerObserver("执行人");
+        ManagerObserver fieldman = new ManagerObserver("现场工作人员");
 
         // 公司经理订阅培训申请通知
         messageManager.subscribe("培训申请", manager);
@@ -19,10 +21,16 @@ public class MessageMiddlewareHQ {
         // 学员订阅报名确认通知
         messageManager.subscribe("报名确认", student);
 
+        // 签到确认通知（
+        messageManager.subscribe("李忠忠签到确认", fieldman);
+        messageManager.subscribe("李月月签到确认", fieldman);
+
         // 发布消息
         messageManager.publish("培训申请", new Message("新的培训申请：技术培训"));
         messageManager.publish("课程信息", new Message("新的课程：Java编程"));
         messageManager.publish("报名确认", new Message("学员张三报名成功"));
+        messageManager.publish("李忠忠签到确认", new Message("李忠忠签到成功"));
+        messageManager.publish("李月月签到确认", new Message("李月月签到成功"));
 
         // 取消订阅
         messageManager.unsubscribe("培训申请", manager);
